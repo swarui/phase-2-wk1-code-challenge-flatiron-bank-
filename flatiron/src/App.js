5055c6c7036eaf0b795afd8b4456cada00018573
swarui <stevewaruim@gmail.com>
2023-10-28 13:35:12 +0300

    Fetch data from the provided API

--- flatiron/src/App.js
+++ flatiron/src/App.js
@@ -1,7 +1,26 @@
-import React, { useState, useEffect } from "react"
-import {v4 as uuidv4 } from "uuid"
-import SearchBar from "./components/SearchBar"
-import Header from "./components/Header"
-import TransactionTable from "./components/TransactionTable"
-import TransactionList from "./components/TransactionList"
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import TransactionForm from "./components/TransactionForm";
+import TransactionTable from "./components/TransactionTable";
+import SearchBar from "./components/SearchBar";
 import "./App.css";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  // Fetch initial data from localStorage when the component mounts.
+  useEffect(() => {
+    const storedTransactions =
+      JSON.parse(localStorage.getItem("transactions")) || [];
+    if (storedTransactions.length === 0) {
+      //  Fetch data from API (http://localhost:8001/transactions)
+      fetch("http://localhost:8001/transactions")
+        .then((response) => response.json())
+        .then((data) => {
+          // Assign unique IDs using uuid
+          const transactionsWithIds = data.map((transaction) => ({
+            ...transaction,
+            id: uuidv4(),
+          }));
+       
