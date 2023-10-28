2c59c60dfcb37bcdbaa7e1d57c46a4fd13c0804a
swarui <stevewaruim@gmail.com>
2023-10-28 13:18:38 +0300

    Imported the necessary components in App.js

---
+++ flatiron/src/App.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react"
+import {v4 as uuidv4 } from "uuid"
+import SearchBar from "./components/SearchBar"
+import Header from "./components/Header"
+import TransactionTable from "./components/TransactionTable"
+import TransactionList from "./components/TransactionList"
+import "./App.css";
