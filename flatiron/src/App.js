64eda9e24149a262d39756e4d6ef88c8d226b66e
swarui <stevewaruim@gmail.com>
2023-10-28 14:36:45 +0300

    Import search bar

--- flatiron/src/App.js
+++ flatiron/src/App.js
@@ -23,4 +23,64 @@ const App = () => {
             ...transaction,
             id: uuidv4(),
           }));
-       
+          setTransactions(transactionsWithIds);
+          localStorage.setItem(
+            "transactions",
+            JSON.stringify(transactionsWithIds)
+          );
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+    } else {
+      // If there is data in localStorage, use it directly
+      setTransactions(storedTransactions);
+    }
+  }, []);
+
+  // Store transaction in localStorage when transactions state changes.
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
+
+  const handleAddTransaction = (newTransaction) => {
+    // Generate a unique ID using uuid
+    const transactionWithId = { ...newTransaction, id: uuidv4() };
+    setTransactions([...transactions, transactionWithId]);
+  };
+
+  const handleDeleteTransaction = (id) => {
+    const updatedTransactions = transactions.filter(
+      (transaction) => transaction.id !== id
+    );
+    setTransactions(updatedTransactions);
+  };
+
+  const handleSort = (sortBy) => {
+    const sortedTransactions = [...transactions];
+    sortedTransactions.sort((a, b) => {
+      if (sortBy === "date") {
+        return new Date(a[sortBy]) - new Date(b[sortBy]);
+      } else if (sortBy === "amount") {
+        return a[sortBy] - b[sortBy]; // Assuming that the amount is a number, not a string
+      } else {
+        return a[sortBy].localeCompare(b[sortBy]);
+      }
+    });
+    setTransactions(sortedTransactions);
+  };
+
+  return (
+    <div>
+      <h1>Bank of Flatiron</h1>
+      <SearchBar onSearch={setSearchTerm} />
+      <TransactionTable
+        transactions={transactions}
+        searchTerm={searchTerm}
+        onDeleteTransaction={handleDeleteTransaction}
+        onSort={handleSort}
+      />
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+    </div>
+  );
+};
+
+export default App;
